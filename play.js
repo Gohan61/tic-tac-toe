4fecf45ad181b2af349df233c2aacdaa8c55769d
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-02-01 16:53:29 +0100

    Clear board after game complete

--- play.js
+++ play.js
@@ -5,7 +5,7 @@ const gameBoard = (function () {
 
   const getItems = () => items;
   const setItems = (index, player) => items.splice(index, 1, player);
-  const clearBoard = () => (items = ["", "", "", "", "", "", "", "", ""]);
+  const clearBoard = () => items.forEach((element, i) => (items[i] = ""));
 
   return { getItems, setItems, clearBoard };
 })();
@@ -42,51 +42,67 @@ function game(playerOneMove, playerTwoMove) {
         break;
       case i1 === "o" && i2 === "o" && i3 === "o":
         console.log(`${playerTwo.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i4 === "x" && i5 === "x" && i6 === "x":
         console.log(`${playerOne.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i4 === "o" && i5 === "o" && i6 === "o":
         console.log(`${playerTwo.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i7 === "x" && i8 === "x" && i9 === "x":
         console.log(`${playerOne.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i7 === "o" && i8 === "o" && i9 === "o":
         console.log(`${playerTwo.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i1 === "x" && i4 === "x" && i7 === "x":
         console.log(`${playerOne.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i1 === "o" && i4 === "o" && i7 === "o":
         console.log(`${playerTwo.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i2 === "x" && i5 === "x" && i8 === "x":
         console.log(`${playerOne.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i2 === "o" && i5 === "o" && i8 === "o":
         console.log(`${playerTwo.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i3 === "x" && i6 === "x" && i9 === "x":
         console.log(`${playerOne.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i3 === "o" && i6 === "o" && i9 === "o":
         console.log(`${playerTwo.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i1 === "x" && i5 === "x" && i9 === "x":
         console.log(`${playerOne.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i1 === "o" && i5 === "o" && i9 === "o":
         console.log(`${playerTwo.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i3 === "x" && i5 === "x" && i7 === "x":
         console.log(`${playerOne.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case i3 === "o" && i5 === "o" && i7 === "o":
         console.log(`${playerTwo.getPlayerName()} wins`);
+        gameBoard.clearBoard("");
         break;
       case roundCount === 3:
         console.log("Draw");
+        gameBoard.clearBoard("");
     }
   }
 }

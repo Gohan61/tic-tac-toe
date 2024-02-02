a24bfc32c7d39bfb34b7132878fa35500029b599
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-02-02 15:26:57 +0100

    Set input names in game + fix form submit
    Feat: added logic so players can enter names so game displays winner
    of the input name at game end
    Fix: changed form event listener to click as the form was not responding
    on first click after clicking 'New Game' when event listener was set to
    'submit'

--- play.js
+++ play.js
@@ -1,4 +1,5 @@
 let roundCount = 0;
+const form = document.querySelector("#playerInfo");
 
 const gameBoard = (function () {
   const items = ["", "", "", "", "", "", "", "", ""];
@@ -19,86 +20,103 @@ function players(name, weapon) {
   return { getPlayerName, getPlayerWeapon, setPlayer };
 }
 
-const playerOne = players("Hendrik", "x");
-playerOne.setPlayer();
-const playerTwo = players("Uwe", "o");
-playerTwo.setPlayer();
+const playerInfo = function () {
+  let playerOne = undefined;
+  let playerTwo = undefined;
+
+  let currentWeapon = playerInfo.playerOne.getPlayerWeapon();
+  return { playerOne, playerTwo, currentWeapon };
+};
+
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+  const playerOneInput = document.querySelector("#playerOne");
+  const playerTwoInput = document.querySelector("#playerTwo");
+
+  playerInfo.playerOne = players(playerOneInput.value, "x");
+  playerInfo.playerOne.setPlayer();
+
+  playerInfo.playerTwo = players(playerTwoInput.value, "o");
+  playerInfo.playerTwo.setPlayer();
+  playerInfo();
+  display();
+});
 
 function game(playerOneMove, playerTwoMove) {
   roundCount++;
 
   if (playerOneMove !== undefined) {
-    gameBoard.setItems(playerOneMove, playerOne.getPlayerWeapon());
+    gameBoard.setItems(playerOneMove, playerInfo.playerOne.getPlayerWeapon());
   }
   if (playerTwoMove !== undefined) {
-    gameBoard.setItems(playerTwoMove, playerTwo.getPlayerWeapon());
+    gameBoard.setItems(playerTwoMove, playerInfo.playerTwo.getPlayerWeapon());
   }
 
   const [i1, i2, i3, i4, i5, i6, i7, i8, i9] = gameBoard.getItems();
 
   switch (true) {
     case i1 === "x" && i2 === "x" && i3 === "x":
-      console.log(`${playerOne.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerOne.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i1 === "o" && i2 === "o" && i3 === "o":
-      console.log(`${playerTwo.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerTwo.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i4 === "x" && i5 === "x" && i6 === "x":
-      console.log(`${playerOne.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerOne.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i4 === "o" && i5 === "o" && i6 === "o":
-      console.log(`${playerTwo.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerTwo.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i7 === "x" && i8 === "x" && i9 === "x":
-      console.log(`${playerOne.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerOne.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i7 === "o" && i8 === "o" && i9 === "o":
-      console.log(`${playerTwo.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerTwo.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i1 === "x" && i4 === "x" && i7 === "x":
-      console.log(`${playerOne.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerOne.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i1 === "o" && i4 === "o" && i7 === "o":
-      console.log(`${playerTwo.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerTwo.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i2 === "x" && i5 === "x" && i8 === "x":
-      console.log(`${playerOne.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerOne.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i2 === "o" && i5 === "o" && i8 === "o":
-      console.log(`${playerTwo.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerTwo.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i3 === "x" && i6 === "x" && i9 === "x":
-      console.log(`${playerOne.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerOne.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i3 === "o" && i6 === "o" && i9 === "o":
-      console.log(`${playerTwo.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerTwo.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i1 === "x" && i5 === "x" && i9 === "x":
-      console.log(`${playerOne.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerOne.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i1 === "o" && i5 === "o" && i9 === "o":
-      console.log(`${playerTwo.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerTwo.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i3 === "x" && i5 === "x" && i7 === "x":
-      console.log(`${playerOne.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerOne.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case i3 === "o" && i5 === "o" && i7 === "o":
-      console.log(`${playerTwo.getPlayerName()} wins`);
+      console.log(`${playerInfo.playerTwo.getPlayerName()} wins`);
       gameBoard.clearBoard("");
       break;
     case roundCount === 9:
@@ -107,25 +125,24 @@ function game(playerOneMove, playerTwoMove) {
   }
 }
 
-const display = (function itemsOnDisplay() {
-  let currentPlayer = playerOne.getPlayerName();
-  let currentWeapon = playerOne.getPlayerWeapon();
+const display = function itemsOnDisplay() {
+  // let currentPlayer = playerInfo.playerOne.getPlayerName();
   const gameBoardDisplay = document.querySelectorAll(".boardItem");
 
   const listenClick = gameBoardDisplay.forEach((el) =>
     el.addEventListener("click", () => {
       if (el.textContent !== "") {
-      } else if (el.textContent === "" && currentWeapon === "x") {
-        el.textContent = currentWeapon;
+      } else if (el.textContent === "" && playerInfo.currentWeapon === "x") {
+        el.textContent = playerInfo.currentWeapon;
         game(parseInt(el.getAttribute("index")), undefined);
-        currentWeapon = playerTwo.getPlayerWeapon();
+        playerInfo.currentWeapon = playerInfo.playerTwo.getPlayerWeapon();
       } else {
-        el.textContent = currentWeapon;
+        el.textContent = playerInfo.currentWeapon;
         game(undefined, parseInt(el.getAttribute("index")));
-        currentWeapon = playerOne.getPlayerWeapon();
+        playerInfo.currentWeapon = playerInfo.playerOne.getPlayerWeapon();
       }
     })
   );
-})();
+};
 
 console.log(gameBoard.getItems());
